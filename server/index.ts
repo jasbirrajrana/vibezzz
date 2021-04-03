901ef0e7dc3273bc04a39118b7eeb1c3f7175a4e
Jasbir Rajrana <68157847+jasbirrajrana@users.noreply.github.com>
2021-04-03 16:13:34 +0530

    development start 03-04-2021

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import chalk from 'chalk';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!!');
+});
+app.listen(PORT, () => {
+  console.log(chalk.yellow.bold(`⚡️ app is in ${process.env.NODE_ENV} mode up on ${PORT} `));
+});
